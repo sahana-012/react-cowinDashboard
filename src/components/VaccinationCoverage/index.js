5a79d838a7258504f67f18fcf1f0bfe9aa9a406c
Sahana <sahanas012@gmail.com>
2022-11-15 22:21:22 +0000

    Cowin Dashboard

---
+++ src/components/VaccinationCoverage/index.js
@@ -0,0 +1,57 @@
+// Write your code here
+import {BarChart, Bar, XAxis, YAxis, Legend} from 'recharts'
+
+import './index.css'
+
+const VaccinationCoverage = props => {
+  const dataFormatter = number => {
+    if (number > 1000) {
+      return `${(number / 1000).toString()}k`
+    }
+    return number.toString()
+  }
+  const {vaccinationCoverageDetails} = props
+
+  return (
+    <div className="vaccination-by-coverage-container">
+      <h1 className="vaccination-by-coverage-heading">Vaccination Coverage</h1>
+      <BarChart
+        width={900}
+        height={400}
+        data={vaccinationCoverageDetails}
+        margin={{
+          top: 5,
+        }}
+      >
+        <XAxis
+          dataKey="vaccineDate"
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 1,
+            fontSize: 15,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <YAxis
+          tickFormatter={dataFormatter}
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 0,
+          }}
+        />
+        <Legend
+          wrapperStyle={{
+            padding: 30,
+            textAlign: 'center',
+            fontSize: 12,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <Bar dataKey="dose1" name="Dose 1" fill="#5a8dee" barSize="20%" />
+        <Bar dataKey="dose2" name="Dose 2" fill="#f54394" barSize="20%" />
+      </BarChart>
+    </div>
+  )
+}
+
+export default VaccinationCoverage
